56a5408a06fb0739ad7ffadc73baac19a8d2af8b
moonbeam87 <dev@devpatel.com>
2020-01-03 09:10:34 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class LevelControl : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Bob"))
+        {
+            SceneManager.LoadScene("Level2");
+        }
+    }
+
+}
